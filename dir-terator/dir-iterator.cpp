cd311a8f216709a6b5db6d7bde817260c86276b1
wcrbrm <viclopata@gmail.com>
2018-11-21 19:13:02 +0200

    dir iterator: showing modification date too

--- dir-terator/dir-iterator.cpp
+++ dir-terator/dir-iterator.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <ctime>
 #include <boost/filesystem.hpp>
 #include <boost/range/iterator_range.hpp>
 
@@ -10,14 +11,22 @@ int main(int argc, char *argv[])
     path p(argc > 1 ? argv[1] : ".");
     if(is_directory(p)) {
         std::cout << p << " contains the following:\n\n";
+        std::time_t timeNow = time(0);
+        int seconds_in_a_day = 60 * 60 * 24;
+
         for(auto& entry : boost::make_iterator_range(directory_iterator(p), {})) {
             if (!is_directory(entry)) {
-                std::string path = entry.path().string();
-                if (path.find("./") == 0) {
-                    boost::system::error_code ec;
-                    boost::uintmax_t filesize = file_size(entry, ec);
-                    std::cout << entry << ", " << filesize << " bytes\n";
-                }
+                std::string path = entry.path().filename().string();
+                //if (path.find("") == 0) {
+                    boost::system::error_code ecSize;
+                    boost::uintmax_t filesize = file_size(entry, ecSize);
+                    
+                    boost::system::error_code ecTime;
+                    std::time_t tm = last_write_time(entry, ecTime);
+
+                    double days = difftime(timeNow, tm) / seconds_in_a_day;
+                    std::cout << path << ", " << filesize << " bytes, modified " << days << " days ago\n";
+                //}
             }
         }
     }
