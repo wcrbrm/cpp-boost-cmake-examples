8c27061b0faeda05ee451cbde74bd4230e16cc27
wcrbrm <viclopata@gmail.com>
2018-11-22 11:54:04 +0200

    unit tests example added

---
+++ unit-tests/include/calc.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Calculator
+{
+public:
+    int plus(int left, int right);
+    int minus(int left, int right);
+};
---
+++ unit-tests/src/calc.cpp
@@ -0,0 +1,12 @@
+#include "calc.h"
+
+int Calculator::plus(int left, int right)
+{
+    return left + right;
+}
+
+int Calculator::minus(int left, int right)
+{
+    return left - right;
+}
+
